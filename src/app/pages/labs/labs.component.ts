d7a0d5dbba0561aaeab10b8c43137e0648fd514a
Sedron2 <joseabad@leadershipschool.edu.pe>
2024-01-12 16:45:22 -0500

    Usado scaffolding, añadido routing

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  title = 'todoapp';
+  tasks = [
+    "Instalar el angular CLI",
+    "Crear el proyecto con el comando ng new",
+    "Crear componentes"
+  ];
+
+  nombre = "Fabian";
+}
